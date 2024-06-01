61b940db8b648e75776063ce98bd5182f80bb163
OyShan1 <ilhamdzaky2007@gmail.com>
2024-06-01 09:29:56 +0700

    new

--- assets/config/connect.php
+++ assets/config/connect.php
@@ -1,8 +1,8 @@
 <?php
 
 $host = "localhost";
-$user = "baraja";
-$password = "bar123";
+$user = "root";
+$password = "123";
 $db = "epres";
 
 $is_connect = mysqli_connect($host,$user,$password,$db);
